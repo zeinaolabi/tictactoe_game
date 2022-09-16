585a0509d15c0db1cf2a2fcdf7e32db803f62a90
Zeina <zeina@Zeinas-MacBook-Pro.local>
2022-09-16 21:51:51 +0300

    Prevented from selecting same cell

--- js/script.js
+++ js/script.js
@@ -9,15 +9,23 @@ const win_com = [
     [3,5,7]
 ]
 const cells = document.querySelectorAll(".cell");
+const isClicked = []
 let firstUserTurn = true;
 
 
 function showCoin(){
+    if(isClicked.includes(this.id)){
+        return
+    }
+
     if(firstUserTurn){
         this.innerHTML = '<img src="images/red.png" alt="red coin">'
         firstUserTurn = false;
+        isClicked.push(this.id);
         return
     }
+    firstUserTurn = true;
+    isClicked.push(this.id);
     this.innerHTML = '<img src="images/yellow.png" alt="red coin">'
 }
 cells.forEach(cell => cell.addEventListener('click', showCoin))
